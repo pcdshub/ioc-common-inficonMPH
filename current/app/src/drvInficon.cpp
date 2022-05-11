509d6a83fcbeab015c929f426c06f359d4d3f1df
janezg-SLAC <janezg@slac.stanford.edu>
2022-05-11 08:53:45 -0700

    adding functions for creating new device

--- current/app/src/drvInficon.cpp
+++ current/app/src/drvInficon.cpp
@@ -13,6 +13,8 @@
 
 /* Asyn includes */
 #include <drvAsynIPPort.h>
+#include <asynPortDriver.h>
+#include <asynOctetSyncIO.h>
 
 #include "drvInficon.h"
 
@@ -22,7 +24,129 @@ using nlohmann::json;
 
 static const char *driverName = "INFICONMPH";
 
+//==========================================================//
+// class devInficon
+//		Holds useful vars for interacting with Inficon MPH RGA****
+//		hardware
+//==========================================================//
+devInficon::devInficon() {
+	/* Initialize members*/
+	pasynUser = NULL;
+	m_portName = NULL;
+	m_hostInfo = NULL;
+	m_connected = false;
+	m_init = false;
+	m_debug = false;
+
+	/* Lets make sure there are no nullptr issues */
+	m_hostInfo = (char*)malloc(1);
+	m_hostInfo[0] = '\0';
+	m_portName = (char*)malloc(1);
+	m_portName[0] = '\0';
+}
+
+devInficon::~devInficon() {
+	if (m_hostInfo)
+		free(m_hostInfo);
+	if (m_portName)
+		free(m_portName);
+	if (m_name)
+		free(m_name);
+	
+	//pasynManager->disconnect(pasynUser);
+    //pasynManager->freeAsynUser(pasynUser);
+    //pasynUser = NULL;
+}
+
+
+devInficon* devInficon::Create(const char* portName, const char* hostInfo) {
+	if (!portName || !hostInfo)
+		return NULL;
+	
+	devInficon* pinficon = new devInficon();
+	
+	/* Free the previously allocated stuff */
+	free(pinficon->m_portName);
+	free(pinficon->m_hostInfo);
+	
+	/* Copy portName */
+	pinficon->m_portName = strdup(portName);
+	/* Copy hostInfo */
+	pinficon->m_hostInfo = strdup(hostInfo);
+	
+    //drvAsynIPPortConfigure("portName","hostInfo",priority,noAutoConnect,noProcessEos)
+	int status = drvAsynIPPortConfigure(pinficon->m_portName, pinficon->m_hostInfo, 0, 0, 0); //I think for the HTTP port the noAutoConnect should be set to 1
+
+	if (status) {
+		util::Error("devInficon::Create(): Unable to configure drvAsynIPPort.");
+		return connected;
+	}
+
+	/* check connection */
+	asynUser* usr = pasynManager->createAsynUser(NULL, NULL);
+	pasynManager->connectDevice(usr, pinficon->m_portName, 0);
+	int connected = 0;
+	pasynManager->isConnected(usr, &connected);
+	pasynManager->disconnect(usr);
+	pasynManager->freeAsynUser(usr);
+
+	if (!connected) {
+		util::Error("devInficon::Create(): Error while connecting to device %s.", name);
+		return NULL;
+	}
+	
+	return pinficon;
+}
+
+//==========================================================//
+// IOCsh functions here
+//==========================================================//
+void configCallFunc(const iocshArgBuf* args) {
+	const char* portName = args[0].sval;
+	const char* ip = args[1].sval;
+	int port = args[2].ival;
+
+	if (!portName) {
+		epicsPrintf("Invalid port name passed.\n");
+		return;
+	}
+
+	if (!ip) {
+		epicsPrintf("Invalid IP passed.\n");
+		return;
+	}
+
+	if (port <= 0) {
+		epicsPrintf("The port %i is invalid.\n", port);
+		return;
+	}
+
+	devInficon* dev;
+	
+	char hostInfo[64];
+	sprintf(hostInfo, "%s:%i", ip, port);
+
+    dev = devInficon::Create(portName, hostInfo);
+	
+	if (!dev) {
+		epicsPrintf("Unable to create device: Unspecified error.\n");
+		return;
+	}
+}
+
+
 int drvInficonRegister() {
+	
+	/* inficonMPHConfigure("name", "ip", port) */
+	{
+		static const iocshArg arg1 = {"Port Name", iocshArgString};
+		static const iocshArg arg2 = {"IP", iocshArgString};
+		static const iocshArg arg3 = {"Port Number", iocshArgInt};
+		static const iocshArg* const args[] = {&arg1, &arg2, &arg3};
+		static const iocshFuncDef func = {"inficonMPHConfigure", 3, args};
+		iocshRegister(&func, configCallFunc);
+	}
+	
 	return 0;
 }
 epicsExportRegistrar(drvInficonRegister);
--- current/app/src/drvInficon.h
+++ current/app/src/drvInficon.h
@@ -6,3 +6,65 @@
 // Date Created: May 10, 2022
 
 //======================================================//
+
+/* Record types */
+
+/* EPICS includes */
+#include <epicsExport.h>
+#include <epicsMath.h>
+#include <epicsStdio.h>
+#include <epicsStdlib.h>
+#include <epicsAssert.h>
+#include <dbAccess.h>
+#include <devSup.h>
+#include <alarm.h>
+#include <epicsString.h>
+#include <dbScan.h>
+#include <boRecord.h>
+#include <iocsh.h>
+#include <callback.h>
+#include <epicsStdio.h>
+#include <errlog.h>
+#include <epicsMessageQueue.h>
+
+#include <asynPortDriver.h>
+
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+#include <functional>
+#include <list>
+
+
+/* Forward declarations */
+class devInficon;
+
+
+class devInficon {
+private:
+    //
+public:
+	/* Device info */
+	asynUser* pasynUser;
+	char* m_portName;
+	char* m_hostInfo;
+
+	bool m_connected;
+	bool m_init;
+
+	/* Enable/disable debugging messages */
+	bool m_debug;
+
+public:
+	drvInficon();
+
+	/* Make  sure to free everything */
+	~drvInficon();
+
+public:
+	//crete instance of inficon device
+	static devInficon* Create(const char* portName, const char* hostinfo);
+
+public:
+	/* Utils for reading/writing */
+};
