49671afeffa2ee901f37da487422e499940bbf44
janezg-SLAC <janezg@slac.stanford.edu>
2022-05-15 18:31:48 -0700

    driver with DrvUser structure

--- current/app/src/drvInficon.cpp
+++ current/app/src/drvInficon.cpp
@@ -23,58 +23,176 @@
 #include <json.hpp>
 using nlohmann::json;
 
-static const char *driverName = "INFICONMPH";
+static const char *driverName = "INFICON";
+
+struct inficonDrvUser_t {
+    inficonCommandType_t commandType;
+    int              len;
+};
 
 //==========================================================//
-// class devInficon
+// class drvInficon
 //		Holds useful vars for interacting with Inficon MPH RGA****
 //		hardware
 //==========================================================//
-devInficon::devInficon() {
-	/* Initialize members*/
-	pasynUser = NULL;
-	m_portName = NULL;
-	m_hostInfo = NULL;
-	m_connected = false;
-	m_init = false;
-	m_debug = false;
+drvInficon::drvInficon(const char *portName, const char* hostInfo)
 
-	/* Lets make sure there are no nullptr issues */
-	m_hostInfo = (char*)malloc(1);
-	m_hostInfo[0] = '\0';
-	m_portName = (char*)malloc(1);
-	m_portName[0] = '\0';
+   : asynPortDriver(portName,
+                    1, /* maxAddr */
+                    asynInt32Mask | asynUInt32DigitalMask | asynInt64Mask | asynFloat64Mask | asynFloat32ArrayMask | asynOctetMask | asynDrvUserMask, /* Interface mask */
+                    asynInt32Mask | asynUInt32DigitalMask | asynInt64Mask | asynFloat64Mask | asynFloat32ArrayMask | asynOctetMask,                   /* Interrupt mask */
+                    ASYN_CANBLOCK | ASYN_MULTIDEVICE, /* asynFlags */
+                    1, /* Autoconnect */
+                    0, /* Default priority */
+                    0), /* Default stack size*/
+
+    inficonExiting_(false),
+    initialized_(false),
+	hostInfo_(epicsStrDup(hostinfo)),
+    portName_(epicsStrDup(portName)),
+    octetPortName_(NULL),
+    isConnected_(false),
+    ioStatus_(asynSuccess),
+    commandType_(NULL),
+    drvUser_(NULL),
+    data_(0),
+    readOK_(0),
+    writeOK_(0),
+    IOErrors_(0),
+    currentIOErrors_(0),
+    maxIOMsec_(0),
+    lastIOMsec_(0)
+
+{
+    int status;
+	int ipConfigureStatus;
+    static const char *functionName = "drvInficon";
+
+    createParam(MODBUS_DATA_STRING,                 asynParamInt32,       &P_Data);
+    createParam(MODBUS_READ_STRING,                 asynParamInt32,       &P_Read);
+    createParam(MODBUS_ENABLE_HISTOGRAM_STRING,     asynParamUInt32Digital, &P_EnableHistogram);
+    createParam(MODBUS_READ_HISTOGRAM_STRING,       asynParamInt32,       &P_ReadHistogram);
+    createParam(MODBUS_HISTOGRAM_BIN_TIME_STRING,   asynParamInt32,       &P_HistogramBinTime);
+    createParam(MODBUS_HISTOGRAM_TIME_AXIS_STRING,  asynParamInt32Array,  &P_HistogramTimeAxis);
+    createParam(MODBUS_POLL_DELAY_STRING,           asynParamFloat64,     &P_PollDelay);
+    createParam(MODBUS_READ_OK_STRING,              asynParamInt32,       &P_ReadOK);
+    createParam(MODBUS_WRITE_OK_STRING,             asynParamInt32,       &P_WriteOK);
+    createParam(MODBUS_IO_ERRORS_STRING,            asynParamInt32,       &P_IOErrors);
+    createParam(MODBUS_LAST_IO_TIME_STRING,         asynParamInt32,       &P_LastIOTime);
+    createParam(MODBUS_MAX_IO_TIME_STRING,          asynParamInt32,       &P_MaxIOTime);
+
+    setIntegerParam(P_ReadOK, 0);
+    setIntegerParam(P_WriteOK, 0);
+    setIntegerParam(P_IOErrors, 0);
+    setIntegerParam(P_LastIOTime, 0);
+    setIntegerParam(P_MaxIOTime, 0);
+	
+	
+	if (octetPortName_ == NULL) octetPortName_ = "";
+    /* Create octet port name */
+	size_t prefixlen = strlen(PORT_PREFIX);
+	size_t len = strlen(portName_) + strlen(PORT_PREFIX) + 1;
+	octetPortName_ = (char*)malloc(len);
+	memcpy(octetPortName_, PORT_PREFIX, prefixlen);
+	memcpy(octetPortName_ + prefixlen, portName_, strlen(portName_) + 1);
+	octetPortName_[len - 1] = '\0';
+	
+    // drvAsynIPPortConfigure("portName","hostInfo",priority,noAutoConnect,noProcessEos)
+	ipConfigureStatus = drvAsynIPPortConfigure(octetPortName_, hostInfo_, 0, 0, 0); //I think for the HTTP port the noAutoConnect should be set to 1
+
+	if (ipConfigureStatus) {
+        asynPrint(pasynUserSelf, ASYN_TRACE_ERROR,
+            "%s::%s, Unable to configure drvAsynIPPort %s",
+            driverName, functionName, octetPortName_);
+        return;
+	}
+
+    /*Allocate data memory, need to do some testing to define max length*/
+    //data_ = (epicsUInt16 *) callocMustSucceed(modbusLength_, sizeof(epicsUInt16), functionName);
+
+    /* Allocate and initialize the default drvUser structure */
+    drvUser_ = (inficonDrvUser_t *) callocMustSucceed(1, sizeof(inficonDrvUser_t), functionName);
+    drvUser_->commandType = commandType_;
+    drvUser_->len = -1;
+
+    /* Connect to asyn octet port with asynOctetSyncIO */
+    status = pasynOctetSyncIO->connect(octetPortName_, 0, &pasynUserOctet_, 0);
+    if (status != asynSuccess) {
+        asynPrint(pasynUserSelf, ASYN_TRACE_ERROR,
+            "%s::%s port %s can't connect to asynOctet on Octet server %s.\n",
+            driverName, functionName, portName_, octetPortName_);
+        return;
+    }
+
+    /* Connect to asyn octet port with asynCommonSyncIO */
+    status = pasynCommonSyncIO->connect(octetPortName_, 0, &pasynUserCommon_, 0);
+    if (status != asynSuccess) {
+        asynPrint(pasynUserSelf, ASYN_TRACE_ERROR,
+            "%s::%s port %s can't connect to asynCommon on Octet server %s.\n",
+        driverName, functionName, portName_, octetPortName_);
+        return;
+     }
+
+    //epicsAtExit(modbusExitCallback, this);
+
+    initialized_ = true;
 }
 
-devInficon::~devInficon() {
-	if (m_hostInfo)
-		free(m_hostInfo);
-	if (m_portName)
-		free(m_portName);
+drvInficon::~drvInficon() {
+	if (hostInfo_)
+		free(hostInfo_);
+	if (portName_)
+		free(portName_);
+	if (octetPortName_)
+		free(octetPortName_);
 	
 	//pasynManager->disconnect(pasynUser);
     //pasynManager->freeAsynUser(pasynUser);
     //pasynUser = NULL;
 }
 
+asynStatus drvInficon::VerifyConnection() {
+	/* asynUsers should be pretty cheap to create */
+	asynUser* usr = pasynManager->createAsynUser(NULL, NULL);
+	usr->timeout = 0.5; /* 500ms timeout */
 
-devInficon* devInficon::Create(const char* portName, const char* hostInfo) {
+	/* Try for connection */
+	pasynManager->connectDevice(usr, this->octetPortName_, 0);
+	int yn = 0;
+	pasynManager->isConnected(usr, &yn);
+	pasynManager->disconnect(usr);
+
+	pasynManager->freeAsynUser(usr);
+
+	return (yn==1) ? asynSuccess : asynError;
+}
+
+drvInficon *drvInficon::Create(const char* portName, const char* hostInfo) {
 	if (!portName || !hostInfo)
 		return NULL;
 
-	devInficon* pinficon = new devInficon();
+	devInficon *pinficon = new devInficon();
 	
 	/* Free the previously allocated stuff */
-	free(pinficon->m_portName);
-	free(pinficon->m_hostInfo);
+	free(pinficon->portName_);
+	free(pinficon->hostInfo_);
+	free(pinficon->octetPortName_);
 	
 	/* Copy portName */
-	pinficon->m_portName = strdup(portName);
+	pinficon->portName_ = strdup(portName);
 	/* Copy hostInfo */
-	pinficon->m_hostInfo = strdup(hostInfo);
+	pinficon->hostInfo_ = strdup(hostInfo);
 	
-    //drvAsynIPPortConfigure("portName","hostInfo",priority,noAutoConnect,noProcessEos)
-	int status = drvAsynIPPortConfigure(pinficon->m_portName, pinficon->m_hostInfo, 0, 0, 0); //I think for the HTTP port the noAutoConnect should be set to 1
+	/* Create octet port name */
+	size_t prefixlen = strlen(PORT_PREFIX);
+	size_t len = strlen(portName) + strlen(PORT_PREFIX) + 1;
+	pinficon->octetPortName_ = (char*)malloc(len);
+	memcpy(pinficon->octetPortName_, PORT_PREFIX, prefixlen);
+	memcpy(pinficon->octetPortName_ + prefixlen, portName, strlen(portName) + 1);
+	pinficon->octetPortName_[len - 1] = '\0';
+	
+    // drvAsynIPPortConfigure("portName","hostInfo",priority,noAutoConnect,noProcessEos)
+	int status = drvAsynIPPortConfigure(pinficon->octetPortName_, pinficon->hostInfo_, 0, 0, 0);//I think for the HTTP port the noAutoConnect should be set to 1
 
 	if (status) {
 		epicsPrintf("devInficon::Create(): Unable to configure drvAsynIPPort.");
@@ -82,12 +200,7 @@ devInficon* devInficon::Create(const char* portName, const char* hostInfo) {
 	}
 
 	/* check connection */
-	asynUser* usr = pasynManager->createAsynUser(NULL, NULL);
-	pasynManager->connectDevice(usr, pinficon->m_portName, 0);
-	int connected = 0;
-	pasynManager->isConnected(usr, &connected);
-	pasynManager->disconnect(usr);
-	pasynManager->freeAsynUser(usr);
+	int connected = drvInficon::VerifyConnection();
 
 	if (!connected) {
 		epicsPrintf("devInficon::Create(): Error while connecting to device %s.", hostInfo);
@@ -97,12 +210,29 @@ devInficon* devInficon::Create(const char* portName, const char* hostInfo) {
 	return pinficon;
 }
 
+extern "C" {
+/*
+** drvInficonConfigure() - create and init an asyn port driver for a Inficon
+**
+*/
+
+/** EPICS iocsh callable function to call constructor for the drvInficon class. */
+asynStatus drvInficonConfigure(const char *portName, const char *hostInfo)
+{
+	if (!portName || !hostInfo)
+		return asynError;
+	
+	new drvInficon(portName, hostInfo);
+	
+	return asynSuccess;
+}
+
 //==========================================================//
 // IOCsh functions here
 //==========================================================//
-void configCallFunc(const iocshArgBuf* args) {
-	const char* portName = args[0].sval;
-	const char* ip = args[1].sval;
+static void drvInficonConfigureCallFunc(const iocshArgBuf* args) {
+	const char *portName = args[0].sval;
+	const char *ip = args[1].sval;
 	int port = args[2].ival;
 
 	if (!portName) {
@@ -119,33 +249,28 @@ void configCallFunc(const iocshArgBuf* args) {
 		epicsPrintf("The port %i is invalid.\n", port);
 		return;
 	}
-
-	devInficon* dev;
 	
 	char hostInfo[64];
-	sprintf(hostInfo, "%s:%i", ip, port);
+	sprintf(hostInfo,"%s:%i HTTP", ip, port);
 
-    dev = devInficon::Create(portName, hostInfo);
-	
-	if (!dev) {
-		epicsPrintf("Unable to create device: Unspecified error.\n");
-		return;
-	}
+	drvInficonConfigure(portName, hostInfo);
 }
 
 
 int drvInficonRegister() {
 	
-	/* inficonMPHConfigure("name", "ip", port) */
+	/* drvInficonConfigure("ASYN_PORT", "IP", PORT_NUMBER) */
 	{
 		static const iocshArg arg1 = {"Port Name", iocshArgString};
 		static const iocshArg arg2 = {"IP", iocshArgString};
 		static const iocshArg arg3 = {"Port Number", iocshArgInt};
 		static const iocshArg* const args[] = {&arg1, &arg2, &arg3};
-		static const iocshFuncDef func = {"inficonMPHConfigure", 3, args};
-		iocshRegister(&func, configCallFunc);
+		static const iocshFuncDef func = {"drvInficonConfigure", 3, args};
+		iocshRegister(&func, drvInficonConfigureCallFunc);
 	}
 	
 	return 0;
 }
 epicsExportRegistrar(drvInficonRegister);
+
+}// extern "C"
--- current/app/src/drvInficon.h
+++ current/app/src/drvInficon.h
@@ -8,8 +8,6 @@
 //======================================================//
 #pragma once
 
-/* Record types */
-
 /* EPICS includes */
 #include <epicsExport.h>
 #include <epicsMath.h>
@@ -21,7 +19,6 @@
 #include <alarm.h>
 #include <epicsString.h>
 #include <dbScan.h>
-#include <boRecord.h>
 #include <iocsh.h>
 #include <callback.h>
 #include <epicsStdio.h>
@@ -37,33 +34,246 @@
 #include <list>
 
 /* Forward declarations */
-class devInficon;
+class drvInficon;
 
-class devInficon {
-private:
-    //
+#define PORT_PREFIX "PORT_"
+
+/* These are the strings that device support passes to drivers via
+ * the asynDrvUser interface.
+ * Drivers must return a value in pasynUser->reason that is unique
+ * for that command.
+ */
+// These are the parameters we register with asynPortDriver
+#define MODBUS_DATA_STRING                "MODBUS_DATA"
+#define MODBUS_READ_STRING                "MODBUS_READ"
+#define MODBUS_ENABLE_HISTOGRAM_STRING    "ENABLE_HISTOGRAM"
+#define MODBUS_READ_HISTOGRAM_STRING      "READ_HISTOGRAM"
+#define MODBUS_HISTOGRAM_BIN_TIME_STRING  "HISTOGRAM_BIN_TIME"
+#define MODBUS_HISTOGRAM_TIME_AXIS_STRING "HISTOGRAM_TIME_AXIS"
+#define MODBUS_POLL_DELAY_STRING          "POLL_DELAY"
+#define MODBUS_READ_OK_STRING             "READ_OK"
+#define MODBUS_WRITE_OK_STRING            "WRITE_OK"
+#define MODBUS_IO_ERRORS_STRING           "IO_ERRORS"
+#define MODBUS_LAST_IO_TIME_STRING        "LAST_IO_TIME"
+#define MODBUS_MAX_IO_TIME_STRING         "MAX_IO_TIME"
+
+// These are the data type strings that are used in the drvUser parameter
+// They are not registered with asynPortDriver
+//Communication
+#define INFICON_IP_STRING                 "IP"
+#define INFICON_MAC_STRING                "MAC"
+#define INFICON_LOGIN_STRING              "LOGIN"
+#define INFICON_ERROR_LOG_STRING          "ERROR_LOG"
+//General control
+#define INFICON_SET_EMI_STRING            "SET_EMI"
+#define INFICON_GET_EMI_STRING            "GET_EMI"
+#define INFICON_SET_EM_STRING             "SET_EM"
+#define INFICON_GET_EM_STRING             "GET_EM"
+#define INFICON_SET_RFGEN_STRING          "SET_RFGEN"
+#define INFICON_GET_RFGEN_STRING          "GET_RFGEN"
+#define INFICON_GET_FAN_STRING            "GET_FAN"
+#define INFICON_SHUTDOWN_STRING           "SHUTDOWN"
+//Sensor info
+#define INFICON_SENS_NAME_STRING          "SENS_NAME"
+#define INFICON_SENS_DESC_STRING          "SENS_DESC"
+#define INFICON_SENS_SN_STRING            "SENS_SN"
+//Status
+#define INFICON_SYST_STAT_STRING          "SYST_STAT"
+#define INFICON_HW_ERROR_STRING           "HW_ERROR"
+#define INFICON_HW_WARN_STRING            "HW_WARN"
+#define INFICON_PWR_ON_TIME_STRING        "PWR_ON_T"
+#define INFICON_EMI_ON_TIME_STRING        "EMI_ON_T"
+#define INFICON_EM_ON_TIME_STRING         "EM_ON_T"
+#define INFICON_EMI_CML_ON_TIME_STRING    "EMI_CML_ON_T"
+#define INFICON_EM_CML_ON_TIME_STRING     "EM_CML_ON_T"
+#define INFICON_EMI_PRESS_TRIP_STRING     "EMI_PRESS_TRIP"
+//Diagnostic data
+#define INFICON_BOX_TEMP_STRING           "BOX_TEMP"
+#define INFICON_ANODE_POTENTIAL_STRING    "ANODE_POTENTIAL"
+#define INFICON_EMI_CURRENT_STRING        "EMI_CURRENT"
+#define INFICON_FOCUS_POTENTIAL_STRING    "FOCUS_POTENTIAL"
+#define INFICON_ELECT_ENERGY_STRING       "ELECT_ENERGY"
+#define INFICON_FIL_POTENTIAL_STRING      "FIL_POTENTIAL"
+#define INFICON_FIL_CURRENT_STRING        "FIL_CURRENT"
+#define INFICON_EM_POTENTIAL_STRING       "EM_POTENTIAL"
+//Measurement
+#define INFICON_GET_PRESS_STRING          "GET_PRESS"
+#define INFICON_GET_SCAN_STRING           "GET_SCAN"
+//Scan info
+#define INFICON_FIRST_SCAN_STRING         "FIRST_SCAN"
+#define INFICON_LAST_SCAN_STRING          "LAST_SCAN"
+#define INFICON_CURRENT_SCAN_STRING       "CURRENT_SCAN"
+#define INFICON_PPSCAN_STRING             "PPSCAN"
+#define INFICON_SCAN_STAT_STRING          "SCAN_STAT"
+//Sensor detector
+#define INFICON_EM_VOLTAGE_MAX_STRING     "EM_V_MAX"
+#define INFICON_EM_VOLTAGE_MIN_STRING     "EM_V_MIN"
+//Sensor filter
+#define INFICON_DWELL_MAX_STRING          "DWELL_MAX"
+#define INFICON_DWELL_MIN_STRING          "DWELL_MIN"
+//Scan setup
+#define INFICON_SET_START_CH_STRING       "SET_START_CH"
+#define INFICON_GET_START_CH_STRING       "GET_START_CH"
+#define INFICON_SET_STOP_CH_STRING        "SET_STOP_CH"
+#define INFICON_GET_STOP_CH_STRING        "GET_STOP_CH"
+#define INFICON_SET_CH_MODE_STRING        "SET_CH_MODE"
+#define INFICON_GET_CH_MODE_STRING        "GET_CH_MODE"
+#define INFICON_SET_CH_PPAMU_STRING       "SET_CH_PPAMU"
+#define INFICON_GET_CH_PPAMU_STRING       "GET_CH_PPAMU"
+#define INFICON_SET_CH_DWELL_STRING       "SET_CH_DWELL"
+#define INFICON_GET_CH_DWELL_STRING       "GET_CH_DWELL"
+#define INFICON_SET_CH_START_MASS_STRING  "SET_CH_START_MASS"
+#define INFICON_GET_CH_START_MASS_STRING  "GET_CH_START_MASS"
+#define INFICON_SET_CH_STOP_MASS_STRING   "SET_CH_STOP_MASS"
+#define INFICON_GET_CH_STOP_MASS_STRING   "GET_CH_STOP_MASS"
+#define INFICON_SET_SCAN_COUNT_STRING     "SET_SCAN_COUNT"
+#define INFICON_GET_SCAN_COUNT_STRING     "GET_SCAN_COUNT"
+#define INFICON_SCAN_START_STRING         "SCAN_START"
+#define INFICON_SCAN_STOP_STRING          "SCAN_STOP"
+
+#define MAX_INFICON_COMMAND_TYPES          73   
+
+typedef enum {
+    dataTypeInt16,
+    dataTypeInt16SM,
+    dataTypeBCDUnsigned,
+    dataTypeBCDSigned,
+    dataTypeUInt16,
+    dataTypeInt32LE,
+    dataTypeInt32LEBS,
+    dataTypeInt32BE,
+    dataTypeInt32BEBS,
+    dataTypeUInt32LE,
+    dataTypeUInt32LEBS,
+    dataTypeUInt32BE,
+    dataTypeUInt32BEBS,
+    dataTypeInt64LE,
+    dataTypeInt64LEBS,
+    dataTypeInt64BE,
+    dataTypeInt64BEBS,
+    dataTypeUInt64LE,
+    dataTypeUInt64LEBS,
+    dataTypeUInt64BE,
+    dataTypeUInt64BEBS,
+    dataTypeFloat32LE,
+    dataTypeFloat32LEBS,
+    dataTypeFloat32BE,
+    dataTypeFloat32BEBS,
+    dataTypeFloat64LE,
+    dataTypeFloat64LEBS,
+    dataTypeFloat64BE,
+    dataTypeFloat64BEBS,
+    dataTypeStringHigh,
+    dataTypeStringLow,
+    dataTypeStringHighLow,
+    dataTypeStringLowHigh,
+    dataTypeZStringHigh,
+    dataTypeZStringLow,
+    dataTypeZStringHighLow,
+    dataTypeZStringLowHigh,
+	MAX_INFICON_COMMAND_TYPES
+} inficonCommandType_t;
+
+struct inficonDrvUser_t;
+
+class drvInficon : public asynPortDriver {
 public:
-	/* Device info */
-	asynUser* pasynUser;
-	char* m_portName;
-	char* m_hostInfo;
-
-	bool m_connected;
-	bool m_init;
-
-	/* Enable/disable debugging messages */
-	bool m_debug;
-
-public:
-	devInficon();
-
+	drvInficon(const char *portName, const char* hostInfo);
+	
 	/* Make  sure to free everything */
-	~devInficon();
+	~drvInficon();
 
+    /* These are the methods that we override from asynPortDriver */
+
+    /* These functions are in the asynCommon interface */
+    virtual void report(FILE *fp, int details);
+    virtual asynStatus connect(asynUser *pasynUser);
+    virtual asynStatus getAddress(asynUser *pasynUser, int *address);
+
+   /* These functions are in the asynDrvUser interface */
+    virtual asynStatus drvUserCreate(asynUser *pasynUser, const char *drvInfo, const char **pptypeName, size_t *psize);
+    virtual asynStatus drvUserDestroy(asynUser *pasynUser);
+
+    /* These functions are in the asynUInt32Digital interface */
+    virtual asynStatus writeUInt32Digital(asynUser *pasynUser, epicsUInt32 value, epicsUInt32 mask);
+    virtual asynStatus readUInt32Digital(asynUser *pasynUser, epicsUInt32 *value, epicsUInt32 mask);
+
+    /* These functions are in the asynInt32 interface */
+    virtual asynStatus writeInt32(asynUser *pasynUser, epicsInt32 value);
+    virtual asynStatus readInt32(asynUser *pasynUser, epicsInt32 *value);
+
+    /* These functions are in the asynInt64 interface */
+    virtual asynStatus writeInt64(asynUser *pasynUser, epicsInt64 value);
+    virtual asynStatus readInt64(asynUser *pasynUser, epicsInt64 *value);
+
+    /* These functions are in the asynFloat64 interface */
+    virtual asynStatus writeFloat64(asynUser *pasynUser, epicsFloat64 value);
+    virtual asynStatus readFloat64(asynUser *pasynUser, epicsFloat64 *value);
+
+    /* These functions are in the asynInt32Array interface */
+    virtual asynStatus readFloat32Array(asynUser *pasynUser, epicsFloat32 *data, size_t maxChans, size_t *nactual);
+    //virtual asynStatus writeFloat32Array(asynUser *pasynUser, epicsFloat32 *data, size_t maxChans);
+
+    /* These functions are in the asynOctet interface */
+    virtual asynStatus writeOctet(asynUser *pasynUser, const char *value, size_t maxChars, size_t *nActual);
+    virtual asynStatus readOctet(asynUser *pasynUser, char *value, size_t maxChars, size_t *nActual, int *eomReason);
+
+    /* These are the methods that are new to this class */
+    inficonCommandType_t getCommandType(asynUser *pasynUser);
+    int getStringLen(asynUser *pasynUser, size_t maxChars);
+    asynStatus doInficonIO(int slave, int function, int start, epicsUInt16 *data, int len);
+    asynStatus readPlcInt32(inficonCommandType_t dataType, int offset, epicsInt32 *value, int *bufferLen);
+    asynStatus writePlcInt32(inficonCommandType_t dataType, int offset, epicsInt32 value, epicsUInt16 *buffer, int *bufferLen);
+    asynStatus readPlcInt64(inficonCommandType_t dataType, int offset, epicsInt64 *value, int *bufferLen);
+    asynStatus writePlcInt64(inficonCommandType_t dataType, int offset, epicsInt64 value, epicsUInt16 *buffer, int *bufferLen);
+    asynStatus readPlcFloat(inficonCommandType_t dataType, int offset, epicsFloat64 *value, int *bufferLen);
+    asynStatus writePlcFloat(inficonCommandType_t dataType, int offset, epicsFloat64  value, epicsUInt16 *buffer, int *bufferLen);
+    asynStatus readPlcString (inficonCommandType_t dataType, int offset, char *value, size_t maxChars, int *bufferLen);
+    asynStatus writePlcString(inficonCommandType_t dataType, int offset, const char *value, size_t maxChars, size_t *nActual, int *bufferLen);
+    bool inficonExiting_;
+	asynStatus verifyConnection();   // Verify connection using asynUser //Return asynSuccess for connect
+/*
 public:
 	//crete instance of inficon device
-	static devInficon* Create(const char* portName, const char* hostinfo);
+	static drvInficon* Create(const char* portName, const char* hostinfo);*/
+protected:
+    /* Values used for pasynUser->reason, and indexes into the parameter library. */
+    int P_Data;
+    int P_Read;
+    int P_EnableHistogram;
+    int P_ReadHistogram;
+    int P_HistogramBinTime;
+    int P_HistogramTimeAxis;
+    int P_PollDelay;
+    int P_ReadOK;
+    int P_WriteOK;
+    int P_IOErrors;
+    int P_LastIOTime;
+    int P_MaxIOTime;
 
-public:
-	/* Utils for reading/writing */
+private:
+    /* Our data */
+    bool initialized_;           /* If initialized successfully */
+	char *portName_;             /* asyn port name for the user driver */
+    char *octetPortName_;        /* asyn port name for the asyn octet port */
+	char *hostInfo_;
+    bool isConnected_;           /* Connection status */
+    asynStatus ioStatus_;        /* I/O error status */
+    asynUser  *pasynUserOctet_;  /* asynUser for asynOctet interface to asyn octet port */
+    asynUser  *pasynUserCommon_; /* asynUser for asynCommon interface to asyn octet port */
+    asynUser  *pasynUserTrace_;  /* asynUser for asynTrace on this port */
+    inficonCommandType_t commandType_;  /* Command type */
+    inficonDrvUser_t *drvUser_;   /* Drv user structure */
+    epicsUInt16 *data_;          /* Memory buffer */
+    char inficonRequest_[MAX_REQUEST_SIZE];       /* Modbus request message */
+    char inficonReply_[MAX_RESPONSE_SIZE];        /* Modbus reply message */
+    int readOK_;
+    int writeOK_;
+    int IOErrors_;
+    int currentIOErrors_; /* IO Errors since last successful writeRead cycle */
+    int maxIOMsec_;
+    int lastIOMsec_;
+
+	/* Enable/disable debugging messages */
+	bool debug_;
 };
