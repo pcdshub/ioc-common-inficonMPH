00eb77e49935554bc4ca2711d4c6530ca07f881d
janezg-SLAC <janezg@slac.stanford.edu>
2022-05-31 11:56:58 -0700

    debug

--- current/app/src/drvInficon.cpp
+++ current/app/src/drvInficon.cpp
@@ -650,12 +650,12 @@ asynStatus drvInficon::readOctet(asynUser *pasynUser, char *value, size_t maxCha
         if (chNumber < 1 || chNumber > MAX_CHANNELS) return asynError;
         sprintf(request,"GET /mmsp/scanSetup/channel/%d/get\r\n"
         "\r\n", chNumber);
-        ioStatus_ = inficonReadWrite(request, data_);
-        if (ioStatus_ != asynSuccess) return(ioStatus_);
-        printf("%s::%s chNumber:%d\n", driverName, functionName, chNumber);
-        if (status != asynSuccess) return(status);
-        status = parseChScanSetup(data_, &chScanSetup_[chNumber]);
-        printf("%s::%s status:%d chMode:%s chDwel:%f chppamu:%d chstartMass:%f chstopMass:%f\n", driverName, functionName, status, chScanSetup_[chNumber].chMode, chScanSetup_[chNumber].chDwell, chScanSetup_[chNumber].chPpamu, chScanSetup_[chNumber].chStartMass, chScanSetup_[chNumber].chStopMass);
+        //ioStatus_ = inficonReadWrite(request, data_);
+        //if (ioStatus_ != asynSuccess) return(ioStatus_);
+        //printf("%s::%s chNumber:%d\n", driverName, functionName, chNumber);
+        //if (status != asynSuccess) return(status);
+        //status = parseChScanSetup(data_, &chScanSetup_[chNumber]);
+        //printf("%s::%s status:%d chMode:%s chDwel:%f chppamu:%d chstartMass:%f chstopMass:%f\n", driverName, functionName, status, chScanSetup_[chNumber].chMode, chScanSetup_[chNumber].chDwell, chScanSetup_[chNumber].chPpamu, chScanSetup_[chNumber].chStartMass, chScanSetup_[chNumber].chStopMass);
     } else {
         asynPrint(pasynUserSelf, ASYN_TRACE_ERROR,
                   "%s::%s port %s invalid pasynUser->reason %d\n",
