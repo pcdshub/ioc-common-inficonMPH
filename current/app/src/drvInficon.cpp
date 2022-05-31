5011b3e095688fdf5949aa3a31bcfa776c5fcc75
janezg-SLAC <janezg@slac.stanford.edu>
2022-05-31 13:19:31 -0700

    debug

--- current/app/src/drvInficon.cpp
+++ current/app/src/drvInficon.cpp
@@ -1208,8 +1208,8 @@ asynStatus drvInficon::parseChScanSetup(const char *jsonData, chScanSetupStruct 
 
 		jstring = j["data"][0]["channelMode"];
         strcpy(chScanSetup->chMode, jstring.c_str());
-        chScanSetup->chStartMass = (double)j["data"][0]["startMass"];
-        chScanSetup->chStopMass = (double)j["data"][0]["stopMass"];
+        //chScanSetup->chStartMass = (double)j["data"][0]["startMass"];
+        //chScanSetup->chStopMass = (double)j["data"][0]["stopMass"];
         chScanSetup->chDwell = j["data"][0]["dwell"];
         chScanSetup->chPpamu = j["data"][0]["ppamu"];
     }
