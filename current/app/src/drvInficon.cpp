b3dd381a4fae4ba698991146b54d586667395e39
janezg-SLAC <janezg@slac.stanford.edu>
2022-05-31 12:13:08 -0700

    debug

--- current/app/src/drvInficon.cpp
+++ current/app/src/drvInficon.cpp
@@ -655,6 +655,7 @@ asynStatus drvInficon::readOctet(asynUser *pasynUser, char *value, size_t maxCha
         printf("%s::%s chNumber:%d\n", driverName, functionName, chNumber);
         //if (status != asynSuccess) return(status);
         status = parseChScanSetup(data_, &chScanSetup_[chNumber]);
+        printf("%s::%s status=%d\n", driverName, functionName, status);
         //printf("%s::%s status:%d chMode:%s chDwel:%f chppamu:%d chstartMass:%f chstopMass:%f\n", driverName, functionName, status, chScanSetup_[chNumber].chMode, chScanSetup_[chNumber].chDwell, chScanSetup_[chNumber].chPpamu, chScanSetup_[chNumber].chStartMass, chScanSetup_[chNumber].chStopMass);
     } else {
         asynPrint(pasynUserSelf, ASYN_TRACE_ERROR,
