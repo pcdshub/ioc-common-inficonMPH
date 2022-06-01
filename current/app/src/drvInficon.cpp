5a1c561bf802bd6c1bff8a3451973bce73e76c2c
janezg-SLAC <janezg@slac.stanford.edu>
2022-06-01 14:04:34 -0700

    debug

--- current/app/src/drvInficon.cpp
+++ current/app/src/drvInficon.cpp
@@ -632,6 +632,7 @@ asynStatus drvInficon::readFloat32Array(asynUser *pasynUser, epicsFloat32 *data,
         if (status != asynSuccess) return(status);
 		*nactual = scanData_->actualScanSize;
 		data = scanData_->scanValues;
+		data[0] = 2;
         printf("%s::%s array0:%e array1:%e array2:%e nElements:%d scanNum:%d\n", driverName, functionName, scanData_->scanValues[0], scanData_->scanValues[1], scanData_->scanValues[2], scanData_->actualScanSize, scanData_->scanNumber);
     } else {
         asynPrint(pasynUserSelf, ASYN_TRACE_ERROR,
