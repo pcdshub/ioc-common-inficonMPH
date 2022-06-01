ef00838296c4b64c49c9190aac238cc6b0bd5cc0
janezg-SLAC <janezg@slac.stanford.edu>
2022-06-01 14:20:33 -0700

    debug

--- current/app/src/drvInficon.h
+++ current/app/src/drvInficon.h
@@ -379,7 +379,7 @@ private:
     sensFiltStruct *sensFilt_;
     chScanSetupStruct *chScanSetup_;
     scanDataStruct *scanData_;
-	double doubletemp_[2501];
+	float doubletemp_[2501];
 };
 
 #endif /* drvInficon_H */
