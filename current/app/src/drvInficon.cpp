a601cc65ad9a732e1a73efbfe87151a6f6d060d1
janezg-SLAC <janezg@slac.stanford.edu>
2022-05-31 17:19:33 -0700

    debug

--- current/app/src/drvInficon.cpp
+++ current/app/src/drvInficon.cpp
@@ -47,7 +47,7 @@ static const char *driverName = "INFICON";
 drvInficon::drvInficon(const char *portName, const char* hostInfo)
 
    : asynPortDriver(portName,
-                    5, /* maxAddr */
+                    MAX_CHANNELS, /* maxAddr */
                     asynInt32Mask | asynUInt32DigitalMask | asynFloat64Mask | asynFloat32ArrayMask | asynOctetMask | asynDrvUserMask, /* Interface mask */
                     asynInt32Mask | asynUInt32DigitalMask | asynFloat64Mask | asynFloat32ArrayMask | asynOctetMask,                   /* Interrupt mask */
                     ASYN_CANBLOCK | ASYN_MULTIDEVICE, /* asynFlags */
