774b964ab7ee8e0851934bb739aacf6453f32775
janezg-SLAC <janezg@slac.stanford.edu>
2022-06-01 12:34:27 -0700

    debug

--- current/app/src/drvInficon.cpp
+++ current/app/src/drvInficon.cpp
@@ -1335,7 +1335,7 @@ asynStatus drvInficon::parseScan(const char *jsonData, float *data, int *scanSiz
         std::vector <float> values(16384);
 		values = j["data"]["values"].get<std::vector<float>>();
         data = &values[0];
-        //printf("%s::%s value0:%e value1:%e\n", driverName, functionName, values[0], values[1]);
+        printf("%s::%s value0:%e value1:%e\n", driverName, functionName, values[0], values[1]);
     }
 	catch (const json::parse_error& e) {
         asynPrint(pasynUserSelf, ASYN_TRACE_ERROR, 
