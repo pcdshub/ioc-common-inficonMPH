8e843eddb606933b53dc23b89f7461bf848f82fe
janezg-SLAC <janezg@slac.stanford.edu>
2022-06-27 22:31:22 -0700

    code fix

--- current/app/src/drvInficon.cpp
+++ current/app/src/drvInficon.cpp
@@ -71,7 +71,7 @@ drvInficon::drvInficon(const char *portName, const char* hostInfo)
     totalPressure_(0),
     pollTime_(DEFAULT_POLL_TIME),
     forceCallback_(true),
-    mainState_(IDLE)
+    mainState_(IDLE),
     startingLeakcheck_(false),
     startingMonitor_(false),
     leakChkValue_(0)
